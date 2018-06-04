002c9a7d9dcd93afdbec3da045d239fbd2415dc9
Guilherme Yambanis Thomaz <gui.yambanis@gmail.com>
2018-06-04 18:36:52 -0300

    Redefining all pages

--- USPAnalytics/app/controllers/pages_controller.rb
+++ USPAnalytics/app/controllers/pages_controller.rb
@@ -7,32 +7,17 @@ class PagesController < ApplicationController
   end
 
   def contact
-    @title = "Contact"
-    @back = 'about'
-    @next = []
   end
 
   def data
-    @title = "Data"
-    @back = 'landing_page'
-    @next = []
   end
 
   def analysis
-    @title = "Analysis"
-    @back = 'landing_page'
-    @next = []
   end
 
   def search
-    @title = "Results page"
-    @back = 'landing_page'
-    @next = []
   end
   
   def glossary
-    @title = "Glossary"
-    @back = 'landing_page'
-    @next = []
   end
 end
